30641327f9c1e9f7a09a550045a3aefa887dd00f
babushkahunter69 <babushkahunter69@gmail.com>
2026-06-15 18:28:29 +0800

    fix product pin preview route config exports

--- app/api/product-pin-preview/[productId]/[pinIndex]/route.ts
+++ app/api/product-pin-preview/[productId]/[pinIndex]/route.ts
@@ -1,1 +1,5 @@
-export { GET, contentType, dynamic, runtime } from '../../../pinterest/product-pin-image-png/[productId]/[pinIndex]/route';
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
+export const contentType = 'image/png';
+
+export { GET } from '../../../pinterest/product-pin-image-png/[productId]/[pinIndex]/route';
